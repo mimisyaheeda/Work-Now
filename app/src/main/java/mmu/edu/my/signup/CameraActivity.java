3686dbf0695ba2309b9666f9a535f8aab6e94e01
MadZhaan <azhandevil95@gmail.com>
2021-02-09 22:24:57 +0800

    Assignment P3 final

--- app/src/main/java/mmu/edu/my/signup/CameraActivity.java
+++ app/src/main/java/mmu/edu/my/signup/CameraActivity.java
@@ -7,11 +7,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -22,49 +24,121 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
+
+import io.grpc.Context;
 
 public class CameraActivity extends AppCompatActivity {
 //SurfaceView surfaceView;
 //Camera camera;
 //SurfaceHolder surfaceHolder;
 //Camera.PictureCallback pictureCallback;
-Button btnCapture;
-ImageView imageView;
+private Button btnCapture, btnUpload;
+private ImageView photo;
+private Bitmap image;
+
+static final int CAMERA_REQUEST_CODE = 1;
+StorageReference mStorage;
+ProgressDialog mProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        imageView=findViewById(R.id.imageView);
-        btnCapture=findViewById(R.id.btnCapture);
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        photo=findViewById(R.id.imageView);
+        btnCapture=findViewById(R.id.btnCapture);
+        btnUpload=findViewById(R.id.btnUpload);
+
+        mProgress =  new ProgressDialog(this);
+
+        /*if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},100);
 
-        }
+        }*/
 
         btnCapture.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,99);
+                startActivityForResult(intent, 0);
+            }
+        });
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                upload();
             }
         });
 
 
     }
 
+    private void upload() {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+         final String random = UUID.randomUUID().toString();
+        StorageReference imageRef = mStorage.child("image/" + random);
+
+        byte[] b = stream.toByteArray();
+        imageRef.putBytes(b)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                       taskSnapshot.getMetadata().getReference().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                           @Override
+                           public void onSuccess(Uri uri) {
+                               Uri downloadUri = uri;
+                           }
+                       });
+                        Toast.makeText(CameraActivity.this, "Photo uploaded", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(CameraActivity.this, "Upload Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==99){
+        if(requestCode==0 && resultCode==RESULT_OK){
+
+            image = (Bitmap)data.getExtras().get("data");
+            photo.setImageBitmap(image);
+
+            /*mProgress.setMessage("Uploading Image...");
+            mProgress.show();
+
+            Uri uri = data.getData();
             Bitmap bitmap=(Bitmap)data.getExtras().get("data");
             imageView.setImageBitmap(bitmap);
+            StorageReference filepath = mStorage.child("feedback").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mProgress.dismiss();
+                    Toast.makeText(CameraActivity.this, "Uploading Finish...", Toast.LENGTH_LONG).show();
+                }
+            });*/
         }
     }
 }
